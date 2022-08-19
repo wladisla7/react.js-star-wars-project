f87d1f701074bd3e9c1f8ad794755f3a07b7d047
f0rest <wladislawfedoseev@gmail.com>
2022-08-20 01:51:43 +0300

    add constant http, https and api people

--- src/constatns/api.js
+++ src/constatns/api.js
@@ -1,4 +1,9 @@
-const SWAPI_ROOT = 'https://swapi.dev/api/';
-const SWAPI_PEOPLE = 'people';
+export const HTTPS = 'https://'
+export const HTTP = 'http://'
 
-export const API_PEOPLE = SWAPI_ROOT + SWAPI_PEOPLE
+export const SWAPI_ROOT = 'swapi.dev/api/';
+export const SWAPI_PEOPLE = 'people';
+
+export const API_PEOPLE = HTTPS + SWAPI_ROOT + SWAPI_PEOPLE
+
+
