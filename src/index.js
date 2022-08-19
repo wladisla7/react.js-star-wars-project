60310f32fa340607019e3f6bd74702a78d22edb7
f0rest <wladislawfedoseev@gmail.com>
2022-08-20 01:52:09 +0300

    change path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './containers/App/App'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,4 +11,3 @@ root.render(
   </React.StrictMode>
 );
 
- 
