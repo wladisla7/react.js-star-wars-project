63d9a33be169ecf9da9e7b309fc6aeae904ed375
f0rest <wladislawfedoseev@gmail.com>
2022-08-19 23:16:03 +0300

    add people page to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import PeoplePage from './containers/PeoplePage'
 import './App.css';
+
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>StarWars</h1>
+      <PeoplePage />
     </div>
   );
 }
