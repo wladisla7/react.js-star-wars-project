33b055e64d1b55ab67defa89e8ac74f112472d11
f0rest <wladislawfedoseev@gmail.com>
2022-08-20 03:48:58 +0300

    add people image in people list container

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import { getApiResource } from '../../utils/network.js';
 import { API_PEOPLE } from '../../constatns/api.js';
-import { getPeopleId } from '../../services/getPeopleData.js';
+import { getPeopleId, getPeopleImage } from '../../services/getPeopleData.js';
+import PeopleList from '../../components/PeopleList'
 
 // import styles from './PeoplePage.module.css'
 
@@ -13,11 +14,14 @@ const PeoplePage = () => {
 
         const peopleList = res.results.map(({ name, url }) => {
             const id = getPeopleId(url);
+            const img = getPeopleImage(id);
             console.log(id);
+            console.log(img)
 
             return {
                 name,
-                url
+                url,
+                img
             }
         })
 
@@ -29,13 +33,8 @@ const PeoplePage = () => {
     }, []);
     return (
         <>
-            {people && (
-                <ul>
-                    {people.map(({ name, url }) =>
-                        <li key={name}>{name}</li>
-                    )}
-                </ul>
-            )}
+            {people && <PeopleList people={people} />
+            }
         </>
     )
 
