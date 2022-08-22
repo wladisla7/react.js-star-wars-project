b78541897699e1b3a765dddc9fbce5f36d08ffd8
f0rest <wladislawfedoseev@gmail.com>
2022-08-22 16:56:10 +0300

    add setErrorrApi in People Page

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { withErrorApi } from '../../hoc-heplers/withErrorApi'
 import { getApiResource } from '../../utils/network.js';
 import { API_PEOPLE } from '../../constatns/api.js';
 import { getPeopleId, getPeopleImage } from '../../services/getPeopleData.js';
@@ -6,26 +7,33 @@ import PeopleList from '../../components/PeopleList'
 
 // import styles from './PeoplePage.module.css'
 
-const PeoplePage = () => {
+const PeoplePage = ({ setEerrorApi }) => {
     const [people, setPeople] = useState(null);
+
 
     const getResource = async (url) => {
         const res = await getApiResource(url);
 
-        const peopleList = res.results.map(({ name, url }) => {
-            const id = getPeopleId(url);
-            const img = getPeopleImage(id);
-            console.log(id);
-            console.log(img)
+        if (res) {
 
-            return {
-                name,
-                url,
-                img
-            }
-        })
+            const peopleList = res.results.map(({ name, url }) => {
+                const id = getPeopleId(url);
+                const img = getPeopleImage(id);
+                console.log(id);
+                console.log(img)
 
-        setPeople(peopleList)
+                return {
+                    name,
+                    url,
+                    img
+                }
+            })
+
+            setPeople(peopleList)
+            setEerrorApi(false);
+        } else {
+            setEerrorApi(true);
+        }
     }
 
     useEffect(() => {
@@ -33,11 +41,11 @@ const PeoplePage = () => {
     }, []);
     return (
         <>
-            {people && <PeopleList people={people} />
-            }
+            <h1>Navigator</h1>
+            {people && <PeopleList people={people} />}
         </>
-    )
 
+    )
 }
 
-export default PeoplePage
+export default withErrorApi(PeoplePage);
