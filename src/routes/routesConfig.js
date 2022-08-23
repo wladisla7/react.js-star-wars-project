55222944000a2c8c40d54f51f2d0267113405f54
f0rest <wladislawfedoseev@gmail.com>
2022-08-24 02:59:58 +0300

    create routers config and add elements with path

---
+++ src/routes/routesConfig.js
@@ -0,0 +1,16 @@
+import PeoplePage from '@containers/PeoplePage';
+import HomePage from '@containers/HomePage';
+
+const routesConfig = [
+    {
+        path: '/',
+        element: <HomePage />
+    },
+    {
+        path: '/people',
+        element: <PeoplePage />
+
+    }
+]
+
+export default routesConfig
