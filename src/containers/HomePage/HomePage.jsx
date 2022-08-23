f49f2d810c7e203ef13f202ab9b945a7186e7b2e
f0rest <wladislawfedoseev@gmail.com>
2022-08-24 02:58:41 +0300

    create home page container

---
+++ src/containers/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import styles from './HomePage.module.css';
+
+const HomePage = () => {
+    return (
+        <>
+            <h1 className="headerText">HomePage</h1>
+        </>
+    )
+}
+
+export default HomePage
