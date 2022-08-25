125bb775d7cf5758575faa494e78335ccf9e30b9
f0rest <wladislawfedoseev@gmail.com>
2022-08-26 00:25:53 +0300

    create  stories of UiButton for StoryBook

---
+++ src/components/UI/UiButton/UiButton.stories.js
@@ -0,0 +1,42 @@
+import UiButton from './UiButton';
+
+export default {
+    title: 'Ui-Kit/UiButton',
+    component: UiButton
+}
+
+const Template = (args) => <UiButton {...args} />;
+
+const props = {
+    text: 'Hello',
+    onClick: () => console.log('Button Click'),
+    disabled: false,
+    theme: 'light',
+    classes: '',
+
+}
+
+export const Light = Template.bind({});
+
+Light.args = {
+    ...props,
+    theme: 'light'
+};
+
+export const Dark = Template.bind({});
+
+Dark.args = {
+    ...props,
+    theme: 'dark'
+};
+
+export const Disable = Template.bind({});
+
+Disable.args = {
+    ...props,
+    disabled: true
+};
+
+
+
+
