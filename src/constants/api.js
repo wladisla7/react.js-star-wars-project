29a06f8227672d756ab0dcfb75efff115245dc82
f0rest <wladislawfedoseev@gmail.com>
2022-08-25 05:31:42 +0300

    add const for param page

--- src/constants/api.js
+++ src/constants/api.js
@@ -7,8 +7,10 @@ export const HTTP = 'http://'
 // swapi
 export const SWAPI_ROOT = 'swapi.dev/api/';
 export const SWAPI_PEOPLE = 'people';
+export const SWAPI_PARAM_PAGE = '/?page=';
 
-export const API_PEOPLE = HTTPS + SWAPI_ROOT + SWAPI_PEOPLE
+
+export const API_PEOPLE = HTTPS + SWAPI_ROOT + SWAPI_PEOPLE + SWAPI_PARAM_PAGE;
 
 
 //visual
