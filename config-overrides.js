58c1646419ce65a8dc491c6b5df0c1f13c041346
f0rest <wladislawfedoseev@gmail.com>
2022-08-25 05:31:02 +0300

    add ui and hooks alias

--- config-overrides.js
+++ config-overrides.js
@@ -4,13 +4,15 @@ const { alias } = require('react-app-rewire-alias')
 module.exports = function override(config, env) {
     alias({
         '@components': 'src/components',
+        '@ui': 'src/components/UI',
         '@containers': 'src/containers',
         '@constants': 'src/constants',
         '@hoc-helpers': 'src/hoc-helpers',
         '@services': 'src/services',
         '@utils': 'src/utils',
         '@routes': 'src/routes',
-        '@static':'src/static',
+        '@static': 'src/static',
+        '@hooks': 'src/hooks',
     })(config);
 
     return config;
