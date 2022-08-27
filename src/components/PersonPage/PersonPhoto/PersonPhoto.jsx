0aa249db3a3378feb0ce7c718816fa52441787f8
f0rest <wladislawfedoseev@gmail.com>
2022-08-27 03:42:39 +0300

    create and work with person photo component, add photo of hero

---
+++ src/components/PersonPage/PersonPhoto/PersonPhoto.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import styles from './PersonPhoto.module.css'
+
+
+const PersonPhoto = ({ personPhoto, personName }) => {
+  return (
+    <div className={styles.container}>
+      <img className={styles.photo} src={personPhoto} alt={personName} />
+    </div>
+  )
+}
+
+PersonPhoto.propTypes = {
+  personPhoto: PropTypes.array
+}
+
+
+
+export default PersonPhoto
