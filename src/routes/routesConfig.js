35b88642744938ae7a810f59fc1d4b05931d1fc2
f0rest <wladislawfedoseev@gmail.com>
2022-08-27 03:38:56 +0300

    add rout for person page

--- src/routes/routesConfig.js
+++ src/routes/routesConfig.js
@@ -1,5 +1,6 @@
-import PeoplePage from '@containers/PeoplePage';
 import HomePage from '@containers/HomePage';
+import PeoplePage from '@containers/PeoplePage';
+import PersonPage from '@containers/PersonPage';
 import NotFoundPage from '@containers/NotFoundPage';
 
 const routesConfig = [
@@ -10,6 +11,11 @@ const routesConfig = [
     {
         path: '/people',
         element: <PeoplePage />
+
+    },
+    {
+        path: '/people/:id/',
+        element: <PersonPage />
 
     },
     {
