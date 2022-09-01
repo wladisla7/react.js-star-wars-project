1ac60c9ad8fae236480301f552b01a9bb890242b
f0rest <wladislawfedoseev@gmail.com>
2022-09-01 11:17:27 +0300

    create person photo component, add dispatch

--- src/components/PersonPage/PersonPhoto/PersonPhoto.jsx
+++ src/components/PersonPage/PersonPhoto/PersonPhoto.jsx
@@ -1,17 +1,61 @@
 import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
+import starmain from './img/favorite.svg';
+import starside from './img/favorite-fill.svg';
+
+import { setPersonToFavorite, removePersonFromFavorite } from '@store/actions'
+
 import styles from './PersonPhoto.module.css'
 
 
-const PersonPhoto = ({ personPhoto, personName }) => {
+const PersonPhoto = ({
+  personId,
+  personPhoto,
+  personName,
+  personFavorite,
+  setPersonFavorite
+}) => {
+
+  const dispatch = useDispatch();
+
+  const dispatchFavoritePeople = () => {
+    if (personFavorite) {
+      dispatch(removePersonFromFavorite(personId));
+      setPersonFavorite(false);
+    } else {
+      dispatch(setPersonToFavorite({
+        [personId]: {
+          name: personName,
+          img: personPhoto
+        }
+      }));
+
+      setPersonFavorite(!personFavorite);
+    }
+
+  }
+
   return (
-    <div className={styles.container}>
-      <img className={styles.photo} src={personPhoto} alt={personName} />
+    <div>
+      <div className={styles.container}>
+        <img className={styles.photo} src={personPhoto} alt={personName} />
+        <input type="image"
+          src={personFavorite ? starside : starmain}
+          onClick={dispatchFavoritePeople}
+          className={styles.favorite}
+          alt="Add to favorite"
+        />
+      </div>
     </div>
   )
 }
 
 PersonPhoto.propTypes = {
-  personPhoto: PropTypes.array
+  personId: PropTypes.string,
+  personPhoto: PropTypes.string,
+  personName: PropTypes.string,
+  personFavorite: PropTypes.bool,
+  setPersonFavorite: PropTypes.func,
 }
 
 
