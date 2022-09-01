b8b0426e8870ae4087f6d6c07193e9f2237d6982
f0rest <wladislawfedoseev@gmail.com>
2022-09-01 11:15:13 +0300

    add favorit component in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import Favorite from '@components/Favorite';
 
 import styles from './Header.module.css'
 
@@ -10,6 +11,7 @@ const Header = () => {
                 <li><NavLink to="/people/?page=1">People</NavLink></li>
                 <li><NavLink to="/not-found">Not Found</NavLink></li>
             </ul>
+            <Favorite />
         </div>
     )
 }
