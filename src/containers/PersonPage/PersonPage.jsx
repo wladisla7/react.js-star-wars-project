cb710b6fb4a3cce5ccd430e2c7c3d819210363ec
f0rest <wladislawfedoseev@gmail.com>
2022-09-07 02:41:43 +0300

    create person page

--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect, Suspense } from 'react'
 import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import PropTypes from 'prop-types'
 
@@ -7,6 +8,7 @@ import PersonInfo from '@components/PersonPage/PersonInfo';
 import PersonPhoto from '@components/PersonPage/PersonPhoto';
 import PersonLinkBack from '@components/PersonPage/PersonLinkBack';
 
+import UiLoading from '@ui/UiLoading'
 
 import { withErrorApi } from '@hoc-helpers/withErrorApi'
 import { getApiResource } from '@utils/network'
@@ -16,18 +18,31 @@ import { API_PERSON } from '@constants/api'
 import styles from './PersonPage.module.css'
 
 
+const PersonFilm = React.lazy(() => import('@components/PersonPage/PersonFilms'));
+
+
+
 const PersonPage = ({ setErrorApi }) => {
-    const [personInfo, setPersonInfo] = useState(null)
+    const [personId, setPersonId] = useState(null)
+    const [personInfo, setPersonInfo] = useState(null);
     const [personName, setPersonName] = useState(null)
     const [personPhoto, setPersonPhoto] = useState(null)
+    const [personFilms, setPersonFilms] = useState(null)
+    const [personFavorite, setPersonFavorite] = useState(false);
+
+    const storeData = useSelector(state => state.favoriteReducer);
 
     const { id } = useParams();
-    console.log(id)
-
 
     useEffect(() => {
-        (async () => {
+        (async () => { 
+            
+            storeData[id] ? setPersonFavorite(true) : setPersonFavorite(false)
+
+            setPersonId(id);
+
             const res = await getApiResource(`${API_PERSON}/${id}`);
+
             if (res) {
                 setPersonInfo([
                     { title: 'Height', data: res.height },
@@ -42,25 +57,40 @@ const PersonPage = ({ setErrorApi }) => {
                 setPersonName(res.name);
                 setPersonPhoto(getPeopleImage(id))
 
+                res.films.length && setPersonFilms(res.films)
+
                 setErrorApi(false)
             } else {
                 setErrorApi(true)
             }
         })();
-    }, [id]);
+    }, []);
     return (
         <>
             <PersonLinkBack />
+
             <div className={styles.wrapper}>
                 <h1 className={styles.personeName}>{personName}</h1>
 
                 <div className={styles.container}>
+
                     <PersonPhoto
+                        personId={personId}
                         personPhoto={personPhoto}
                         personName={personName}
+                        personFavorite={personFavorite}
+                        setPersonFavorite={setPersonFavorite}
 
                     />
+
                     {personInfo && <PersonInfo personInfo={personInfo} />}
+
+                    {personFilms &&
+                        <Suspense fallback={<UiLoading />}>
+                            <PersonFilm personFilms={personFilms} />
+                        </Suspense>
+                    }
+
                 </div>
 
             </div>
