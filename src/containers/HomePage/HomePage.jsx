89dfb644caf4b5da53bd90fb66c004f427562007
f0rest <wladislawfedoseev@gmail.com>
2022-09-07 02:42:22 +0300

    add choose side component in home page

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,9 +1,12 @@
+import ChooseSide from '@components/HomePage/ChooseSide';
+
 import styles from './HomePage.module.css';
 
 const HomePage = () => {
     return (
         <>
-            <h1 className="headerText">HomePage</h1>
+            <h1 className="headerText">Choose your side</h1>
+            <ChooseSide />
         </>
     )
 }
